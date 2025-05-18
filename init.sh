72e4cd6466a401aa9f5b2acc92f38d629c90bffd
DmitriyKukuev <dmitrii.kukuiev@mail.ru>
2025-05-18 15:02:34 +0700

    - node из docker-compose

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ docker exec php-fpm composer install
 docker exec php-fpm php artisan migrate
 docker exec php-fpm php artisan l5-swagger:generate
 
-npm ci && npm run build
+cd ./Front && npm ci && npm run build
