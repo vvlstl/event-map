166b2807f20064589c7ef1e9ee6191ac4dc5ddfa
DmitriyKukuev <dmitrii.kukuiev@mail.ru>
2025-05-18 08:33:11 +0700

    * генерация доки апи

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ docker compose up -d --build
 
 docker exec php-fpm composer install
 docker exec php-fpm php artisan migrate
+docker exec php-fpm php artisan l5-swagger:generate
 
 docker exec frontend npm ci
 docker exec frontend npm run build
